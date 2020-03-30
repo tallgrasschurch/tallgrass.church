342619a76e3edd089c4deae7945e77a882377dac
Nate Dillon <natedillon@gmail.com>
2020-03-30 16:58:23 -0500

    Enable CSS passthrough copy

--- v2/.eleventy.js
+++ v2/.eleventy.js
@@ -2,9 +2,13 @@ module.exports = function(eleventyConfig) {
   // Layout aliases
   eleventyConfig.addLayoutAlias('default', 'layouts/default.njk')
 
+  // Passthrough copy
+  eleventyConfig.addPassthroughCopy('src/css')
+
   return {
     dir: {
       input: 'src'
-    }
+    },
+    passthroughFileCopy: true
   }
 }
