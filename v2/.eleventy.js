a15d633c8a67fc86e3d12fa5494c8342d9ad1f2d
Nate Dillon <natedillon@gmail.com>
2020-03-31 14:22:57 -0500

    Setting up Sass compiling/watching

--- v2/.eleventy.js
+++ v2/.eleventy.js
@@ -2,9 +2,6 @@ module.exports = function(eleventyConfig) {
   // Layout aliases
   eleventyConfig.addLayoutAlias('default', 'layouts/default.njk')
 
-  // Passthrough copy
-  eleventyConfig.addPassthroughCopy('src/css')
-
   return {
     dir: {
       input: 'src'
