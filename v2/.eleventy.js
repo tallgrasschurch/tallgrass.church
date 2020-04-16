de602151c6d3d2903d8b37998020ec1906a191fd
Nate Dillon <natedillon@gmail.com>
2020-04-16 14:32:17 -0500

    Added the logo

--- v2/.eleventy.js
+++ v2/.eleventy.js
@@ -2,6 +2,9 @@ module.exports = function(eleventyConfig) {
   // Layout aliases
   eleventyConfig.addLayoutAlias('default', 'layouts/default.njk')
 
+  // Copy images directory to output
+  eleventyConfig.addPassthroughCopy('src/img')
+
   return {
     dir: {
       input: 'src'
