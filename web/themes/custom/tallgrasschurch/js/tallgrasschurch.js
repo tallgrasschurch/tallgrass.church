77901f0322f661fa1973803561e62697188de35d
Nate Dillon <natedillon@gmail.com>
2020-07-23 13:51:21 -0500

    Set up media groups for Drupal layouts

--- web/themes/custom/tallgrasschurch/js/tallgrasschurch.js
+++ web/themes/custom/tallgrasschurch/js/tallgrasschurch.js
@@ -13,69 +13,75 @@ document.documentElement.classList.remove('no-js')
 // Media group
 // -------------------------
 
-// Process all media group items
-var mediaGroup = document.querySelectorAll('.media-group')
-mediaGroup.forEach( function(group) {
+// Process media groups
+const processMediaGroup = (container, headingsSelector) => {
 
-  // Get the headings
-  var headings = group.querySelectorAll('.field__label')
+  // Get all the media group containers and process them
+  const mediaGroup = document.querySelectorAll(container)
+  mediaGroup.forEach( (group) => {
 
-  // Create the media group nav element
-  var navElement = document.createElement('ul')
-  navElement.classList.add('media-group__nav')
+    // Get the headings
+    const headings = group.querySelectorAll(headingsSelector)
 
-  // Create the nav items
-  headings.forEach( function(heading, i) {
+    // Create the media group nav element
+    let navElement = document.createElement('ul')
+    navElement.classList.add('media-group-nav')
 
-    // Add the heading item to the nav
-    var navItem = document.createElement('li')
-    if (i === 0) {
-      navItem.classList.add('is-active')
-    }
-    var navItemBtn = document.createElement('button')
-    navItemBtn.setAttribute('type', 'button')
-    navItemBtn.appendChild(document.createTextNode(heading.textContent))
-    navItem.appendChild(navItemBtn)
-    navElement.appendChild(navItem)
+    // Create the nav items
+    headings.forEach( (heading, i) => {
 
-    // Hide the heading
-    heading.style.display = 'none'
-  })
-
-  // Insert the nav
-  group.insertBefore(navElement, group.firstChild)
-
-  // Hide all sections other than the first
-  var sections = group.querySelectorAll('.field')
-  sections.forEach( function(section, i) {
-    if (i !== 0) {
-      section.style.display = 'none'
-    }
-  })
-
-  // Handle nav items
-  var nav = group.querySelectorAll('.media-group__nav')
-  var navItems = nav[0].querySelectorAll('li')
-  navItems.forEach( function(item, i) {
-    var btn = item.querySelectorAll('button')
-    btn[0].addEventListener('click', function() {
-
-      // Cancel function if the item is currently active
-      if (item.classList.contains('is-active')) {
-        return
+      // Add the heading item to the nav
+      let navItem = document.createElement('li')
+      if (i === 0) {
+        navItem.classList.add('is-active')
       }
+      let navItemBtn = document.createElement('button')
+      navItemBtn.setAttribute('type', 'button')
+      navItemBtn.appendChild(document.createTextNode(heading.textContent))
+      navItem.appendChild(navItemBtn)
+      navElement.appendChild(navItem)
 
-      // Make this button active
-      nav[0].querySelectorAll('.is-active')[0].classList.remove('is-active')
-      item.classList.add('is-active')
+      // Hide the heading
+      heading.style.display = 'none'
+    })
 
-      // Hide all sections
-      sections.forEach( function(section, i) {
+    // Insert the nav
+    group.insertBefore(navElement, group.firstChild)
+
+    // Hide all sections other than the first
+    let sections = group.querySelectorAll('.field')
+    sections.forEach( function(section, i) {
+      if (i !== 0) {
         section.style.display = 'none'
-      })
+      }
+    })
 
-      // Show the proper section content
-      sections[i].style.display = 'block'
+    // Handle nav items
+    let nav = group.querySelectorAll('.media-group-nav')
+    let navItems = nav[0].querySelectorAll('li')
+    navItems.forEach( function(item, i) {
+      let btn = item.querySelectorAll('button')
+      btn[0].addEventListener('click', function() {
+
+        // Cancel function if the item is currently active
+        if (item.classList.contains('is-active')) {
+          return
+        }
+
+        // Make this button active
+        nav[0].querySelectorAll('.is-active')[0].classList.remove('is-active')
+        item.classList.add('is-active')
+
+        // Hide all sections
+        sections.forEach( function(section, i) {
+          section.style.display = 'none'
+        })
+
+        // Show the proper section content
+        sections[i].style.display = 'block'
+      })
     })
   })
-})
+}
+
+processMediaGroup('.layout--media', '.field__label')
